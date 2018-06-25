ce0f57b248f567a4ac9ad51cbf84c404a42d7514
nitinvinayak <f20170415@gmail.com>
2018-06-26 00:05:59 +0530

    hosting webpage locally

--- django/firstProject/firstProject/urls.py
+++ django/firstProject/firstProject/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.urls import path
 from firstApp import views
 
 urlpatterns = [
     url(r'^$',views.index,name="index"),
-    path('admin/', admin.site.urls),
+    url(r'^firstPage',include('firstApp.urls')),
+    path('admin/', admin.site.urls)
 ]
