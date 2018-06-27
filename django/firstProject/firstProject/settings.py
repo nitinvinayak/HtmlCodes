d457598c09f72d012b00696cf08f1400f32acd69
nitinvinayak <f20170415@gmail.com>
2018-06-28 00:12:07 +0530

    Adding templates folder and index.html

--- django/firstProject/firstProject/settings.py
+++ django/firstProject/firstProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'firstProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
