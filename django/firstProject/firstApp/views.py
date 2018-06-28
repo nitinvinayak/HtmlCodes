6edcd2f79b9311c33483e552fd6b89bb03be1e2a
nitinvinayak <f20170415@gmail.com>
2018-06-29 00:21:37 +0530

    connecting index.html to server

--- django/firstProject/firstApp/views.py
+++ django/firstProject/firstApp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(Request):
-    return HttpResponse("Hello World!")
+    my_dict={"insert_me":"Hello I am from index.html"}
+    return render(Request,'index.html',context=my_dict)
