00fbe37f579b2ca8a89a845527a2aa7bdf5412c3
nitinvinayak <f20170415@gmail.com>
2018-07-02 17:24:34 +0530

    creating anew repository for django project

--- django/firstProject/firstApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from firstApp.models import AccessRecords,Topic,Webpage
-
-admin.site.register(AccessRecords)
-admin.site.register(Topic)
-admin.site.register(Webpage)
